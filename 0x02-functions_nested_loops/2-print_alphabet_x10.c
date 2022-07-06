72299a01f1d296b8c24ecc2046702169ee932d43
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2022-07-05 17:14:20 -0700

    0x02-function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * Main - Prints 10x the alphabet in lowercase + a new line
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int counter = 1;
+
+	while (counter <= 10)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			_putchar('\n');
+			letter++;
+		}
+		counter++;
+	}
+}
