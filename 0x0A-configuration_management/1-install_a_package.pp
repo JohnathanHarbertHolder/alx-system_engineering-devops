54bd4d2db51ddba29ae5846c856360c10df01695
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2023-01-11 19:47:32 +0000

    ALX

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install the puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
